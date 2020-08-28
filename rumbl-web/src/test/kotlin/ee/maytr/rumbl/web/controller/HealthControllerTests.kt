ca6a0ae636d529d8ba48a37b9f11a922eeb44c8c
json <json@healingpaper.com>
2020-08-28 19:25:47 +0900

    Test controller with MockMvc

--- rumbl-web/src/test/kotlin/ee/maytr/rumbl/web/controller/HealthControllerTests.kt
+++ rumbl-web/src/test/kotlin/ee/maytr/rumbl/web/controller/HealthControllerTests.kt
@@ -1,21 +1,30 @@
 package ee.maytr.rumbl.web.controller
 
-import org.assertj.core.api.Assertions.assertThat
+import ee.maytr.rumbl.domain.service.HealthService
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
-import org.springframework.http.HttpStatus
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class HealthControllerTests(
-    @Autowired private val restTemplate: TestRestTemplate
-) {
+@WebMvcTest(HealthController::class)
+class HealthControllerTests {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @MockBean
+    private lateinit var healthService: HealthService
+
     @Test
     fun `health returns "healthy"`() {
-        val entity = restTemplate.getForEntity<String>("/health")
-        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).isEqualTo("healthy")
+        `when`(healthService.get()).thenReturn("healthy")
+
+        mockMvc.perform(get("/health"))
+            .andExpect(status().isOk)
+            .andExpect(content().string("healthy"))
     }
 }
