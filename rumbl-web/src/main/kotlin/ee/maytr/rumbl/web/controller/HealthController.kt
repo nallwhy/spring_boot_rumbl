f5eb0368f959426b28c2606dd94072574a1ec45e
json <json@healingpaper.com>
2020-10-31 23:51:06 +0900

    Add debug log to HealthController:health/0

--- rumbl-web/src/main/kotlin/ee/maytr/rumbl/web/controller/HealthController.kt
+++ rumbl-web/src/main/kotlin/ee/maytr/rumbl/web/controller/HealthController.kt
@@ -1,6 +1,7 @@
 package ee.maytr.rumbl.web.controller
 
 import ee.maytr.rumbl.domain.service.HealthService
+import mu.KotlinLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController
 class HealthController(
     private val healthService: HealthService
 ) {
+    companion object {
+        private val logger = KotlinLogging.logger {}
+    }
+
     @GetMapping("/health")
     fun health(): String {
+        logger.debug { "health" }
+
         return healthService.get()
     }
 }
