7ee0dae4a32fdbc4cacd494a7f3242b3a4743ddb
Json <nallwhy@gmail.com>
2020-12-06 21:11:37 +0900

    Upgrade Kotlin version to 1.4.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("org.springframework.boot") version "2.3.1.RELEASE" apply false
     id("io.spring.dependency-management") version "1.0.9.RELEASE" apply false
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.spring") version "1.3.72" apply false
+    kotlin("jvm") version "1.4.20"
+    kotlin("plugin.spring") version "1.4.20" apply false
 }
 
 allprojects {
