16cfbdfc2a01fa0b304cee7853c7f053e790b0d6
Json <nallwhy@gmail.com>
2020-12-06 21:14:02 +0900

    Upgrade Spring version to 2.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.springframework.boot") version "2.3.1.RELEASE" apply false
-    id("io.spring.dependency-management") version "1.0.9.RELEASE" apply false
+    id("org.springframework.boot") version "2.4.0" apply false
+    id("io.spring.dependency-management") version "1.0.10.RELEASE" apply false
     kotlin("jvm") version "1.4.20"
     kotlin("plugin.spring") version "1.4.20" apply false
 }
