b6beabb5a6da6c492ade6f6c0e3b40c63fdd3b36
Json <nallwhy@gmail.com>
2021-02-04 16:28:01 +0900

    Use UserExposedDAO instead of UserJDBCDAO in UserController

--- rumbl-web/src/main/kotlin/ee/maytr/rumbl/web/controller/UserController.kt
+++ rumbl-web/src/main/kotlin/ee/maytr/rumbl/web/controller/UserController.kt
@@ -3,6 +3,7 @@ package ee.maytr.rumbl.web.controller
 import ee.maytr.rumbl.domain.context.accounts.dao.UserDAO
 import ee.maytr.rumbl.web.dto.user.request.UserCreateRequest
 import ee.maytr.rumbl.web.dto.user.response.UserResponse
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UserController(
-    private val userDAO: UserDAO
+    @Qualifier("userExposedDAO") private val userDAO: UserDAO
 ) {
     @GetMapping("/users/{id}")
     fun get(@PathVariable("id") id: Long): UserResponse? {
